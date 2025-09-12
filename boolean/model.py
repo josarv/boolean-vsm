a618da18b98d82d48420d6da0de19d0082b30278
Iosif Arvanitis <58145194+josarv@users.noreply.github.com>
2025-09-12 06:26:03 +0300

    write vsm

--- boolean/model.py
+++ boolean/model.py
@@ -15,27 +15,27 @@ class BooleanIRModel:
             parser: Parser | None = None,
             index: InvertedIndex | None = None,
     ):
-        self.preprocessing_pipeline = preprocessing_pipeline
-        self.query_pipeline: Callable[[str], str] = preserve_boolean_operators(preprocessing_pipeline)
-        self.parser = parser or RecursiveDescentParser()
-        self.ast_pipeline: Callable[[ASTNode], ASTNode] = compose(
+        self._preprocessing_pipeline = preprocessing_pipeline
+        self._query_pipeline: Callable[[str], str] = preserve_boolean_operators(preprocessing_pipeline)
+        self._parser = parser or RecursiveDescentParser()
+        self._ast_pipeline: Callable[[ASTNode], ASTNode] = compose(
             fold_constants,
             flatten_nested_operators,
             deduplicate_operands,
             simplify_tautologies_contradictions,
         )
-        self.index = index or SimpleInvertedIndex(posting_list_factory=lambda: SetPostingList())
+        self._index = index or SimpleInvertedIndex(posting_list_factory=lambda: SetPostingList())
 
     def index_document(self, filename: str, content: str) -> None:
-        tokens = self.preprocessing_pipeline(content)
-        self.index.add_document(filename, tokens)
+        tokens = self._preprocessing_pipeline(content)
+        self._index.add_document(filename, tokens)
 
     def query(self, query_string: str) -> List[str]:
-        processed_query = self.query_pipeline(query_string)
-        ast = self.parser.parse(processed_query)
-        ast.root = self.ast_pipeline(ast.root)
-        result_postings = ast.root.evaluate(self.index)
-        return list(self.index.postings_to_filenames(result_postings))
+        processed_query = self._query_pipeline(query_string)
+        ast = self._parser.parse(processed_query)
+        ast.root = self._ast_pipeline(ast.root)
+        result_postings = ast.root.evaluate(self._index)
+        return list(self._index.postings_to_filenames(result_postings))
 
     def pretty_query(self, query_string: str) -> str:
         results = self.query(query_string)
--- vsm/model.py
+++ vsm/model.py
@@ -1,13 +1,26 @@
+from typing import Callable, Iterable
+from .index import InvertedIndex, SimpleInvertedIndex
+from .postings import DictionaryPostingList
+from .eval import compute_query_vector, compute_cosine_similarities
+
 class VSM:
-    def __init__(self):
-        pass
+    def __init__(
+            self,
+            preprocessing_pipeline: Callable[[str], list[str]],
+            index: InvertedIndex | None = None,
+    ):
+        self._preprocessing_pipeline = preprocessing_pipeline
+        self._index = index or SimpleInvertedIndex(posting_list_factory=lambda: DictionaryPostingList())
 
-    def index_collection(self):
-        pass
+    def index_collection(self, collection: dict[str, Iterable[str]]) -> None:
+        self._index.build(collection)
 
-    def query(self, query_string: str) -> list[tuple[str, float]]:
-        pass
-        # compute query vector
-        # compute cosine similarities
-        # return ranked list of (filename, score) using self.index.postings_to_filenames
-        # filter top k results
+    def query(self, query_string: str, top_k: int | None = None) -> list[tuple[str, float]]:
+        tokens = self._preprocessing_pipeline(query_string)
+        query_vector = compute_query_vector(self._index, tokens)
+        similarities = compute_cosine_similarities(self._index, query_vector)
+        ranked_results = sorted(similarities, key=lambda x: x[1], reverse=True)
+        if top_k is not None:
+            ranked_results = ranked_results[:top_k]
+        ranked_files = [(self._index.document_id_to_filename(doc_id), score) for doc_id, score in ranked_results]
+        return ranked_files

0e0f9d0215d3074c3facf91b752028599a84fb97
Iosif Arvanitis <58145194+josarv@users.noreply.github.com>
2025-09-12 06:37:38 +0300

    its alive

--- project/driver.py
+++ project/driver.py
@@ -1,6 +1,7 @@
 from project.preprocess import tokenize, remove_stopwords
 from boolean.utility import compose
 from boolean.model import BooleanIRModel
+from vsm.model import VSM
 
 from glob import glob
 from os import path
@@ -12,18 +13,30 @@ preprocess = compose(
 )
 
 boolean = BooleanIRModel(preprocessing_pipeline=preprocess)
+vsm = VSM(preprocessing_pipeline=preprocess)
 
 document_directory = "../data/documents"
 
-# glob files in the directory and index them
+# def add_or_between_terms(query: str) -> str:
+#     return ''.join(" || " if char.isspace() else char for char in query)
+#
+# # glob files in the directory and index them
+# for filepath in glob(path.join(document_directory, "*")):
+#     if path.isfile(filepath):
+#         with open(filepath, "r") as file:
+#             content = file.read()
+#             filename = path.basename(filepath)
+#             boolean.index_document(filename, content)
+
+# same for vsm
+collection = {}
 for filepath in glob(path.join(document_directory, "*")):
     if path.isfile(filepath):
         with open(filepath, "r") as file:
             content = file.read()
             filename = path.basename(filepath)
-            boolean.index_document(filename, content)
+            tokens = preprocess(content)
+            collection[filename] = tokens
+vsm.index_collection(collection)
 
-def add_or_between_terms(query: str) -> str:
-    return ''.join(" || " if char.isspace() else char for char in query)
-
-print(boolean.pretty_query(add_or_between_terms("pseudomonas aeruginosa")))
+print(vsm.pretty_query("Are there abnormalities of taste in CF patients"))
